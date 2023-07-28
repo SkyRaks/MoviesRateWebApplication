c9e661d82c0d4db605c9a2067bcc0f368e477d4f
SkyRaks <nano.volosin2016@gmail.com>
2023-07-27 21:54:06 -0400

    Modified PORT settings

--- src/main/java/com/example/MoviesRate/MoviesRateApplication.java
+++ src/main/java/com/example/MoviesRate/MoviesRateApplication.java
@@ -3,11 +3,17 @@ package com.example.MoviesRate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class MoviesRateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoviesRateApplication.class, args);
+//		SpringApplication.run(MoviesRateApplication.class, args);
+		int port = Integer.parseInt(System.getenv("PORT"));
+		SpringApplication app = new SpringApplication(MoviesRateApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", String.valueOf(port)));
+		app.run(args);
 	}
 
 }
